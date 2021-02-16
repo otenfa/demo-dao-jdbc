fa41d1b2ccd0f9925874d69e85fd43951bbb076f
otenfa <afneto1960@gmail.com>
2021-02-16 17:24:09 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,32 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller dep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller dep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer Id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
