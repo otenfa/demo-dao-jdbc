ba753bbdd378f4e179f672dc0bb4f1f92437579c
otenfa <afneto1960@gmail.com>
2021-03-15 16:09:12 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -24,12 +25,16 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		System.out.println("\n=== TESTE findAll === ");
+		System.out.println("\n=== TESTE seller findAll === ");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-
+		
+		System.out.println("\n=== TESTE seller insert === ");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new Seller = " + newSeller.getId());
 	
 	}
 
